e1029427d9f01f4ed8cccd3686da136cdaea93e0
Ocup <ocup0311@gmail.com>
2022-02-16 20:53:13 +0800

    [NOTE] add some note for java learning

--- Test.java
+++ Test.java
@@ -1,5 +1,16 @@
 public class Test {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    static int myIntMethod(int x) {
+        return 5 + x;
+    }
+
+
+    static void myMethod(String fname, int age) {
+        int trueAge = myIntMethod(age);
+        System.out.println(fname + " is " + trueAge);
+    }
+
+    public static void main(String[] args) {
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
+    }
 }
